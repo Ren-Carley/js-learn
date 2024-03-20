61262c03b80195b408dc4ee99efaf852b6abd484
Ren-Carley <ceciliamukuhi772@gmail.com>
2024-03-20 13:37:43 +0300

    added changes

---
+++ evenOdd.js
@@ -0,0 +1,10 @@
+function joe(number) {
+    if (number % 2 == 0) {
+        console.log("This number is even");
+    } else {
+        console.log("This number is odd");
+    }
+}
+
+/**Invoking a function */
+joe(3);
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Im in the js file");
+function checkEvenOrOdd(number){
+if (number % 2 === 0) {
+    return "even";
+} else {
+    return "odd";
+}
+let num1= 10;
+console.log ( checkEvenOrOdd);
+}
